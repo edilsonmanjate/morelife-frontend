c00b06ce9f1146de9d6706ed96861d3d1e1ce4fc
Edilson Manjate <edilsonsh.manjate@gmail.com>
2024-12-20 01:45:43 +0200

    loginfinal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,41 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DonatorComponent } from './pages/donator/donator.component';
+import { DonationComponent } from './pages/donation/donation.component';
 
 export const routes: Routes = [
     {
         path:'',
-        redirectTo : 'home',
+        redirectTo : 'login',
         pathMatch : 'full'
     },
     {
-        path:'home',
-        component : HomeComponent
+        path:'login',
+        component : LoginComponent
     },
+    {
+        path:'',
+        component : LayoutComponent,
+        children : [
+            {
+                path:'home',
+                component : HomeComponent
+            },
+            {
+                path:'donator',
+                component : DonatorComponent
+            },
+            {
+             path:'donatoin',
+                component : DonationComponent
+            },
+        ]   
+    },
+    {
+        path:'donator',
+        component : HomeComponent
+    }
 
 ];
